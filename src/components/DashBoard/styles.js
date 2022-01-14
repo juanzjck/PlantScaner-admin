5f6f79e04576230541fb566850ec465c56666fa6
Jordan Borja <www.jrdnbrj@gmail.com>
2022-01-14 15:55:50 -0500

    Fixed DateTime Format

--- src/components/DashBoard/styles.js
+++ src/components/DashBoard/styles.js
@@ -9,19 +9,58 @@ export const WrappDashboard=styled.div`
     border-radius:20px;
     padding:10px 10px;
 `
-export const WrappNumberOfNotes=styled.div`
-    height:100%;
-    width:100%;
-    display:flex;
-    justify-content: space-between;
-`
-export const NumberOfNotes=styled.h2`
-    font-size:55px;
-`
 
 export const WrapContainer=styled.div`
     display:flex;
     justify-content:center;
     align-items:center;
     min-height:50px;
+`
+
+export const Row = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    margin-bottom:10px;
+`
+
+export const Col = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    margin-bottom:10px;
+    margin: 2rem;
+    border-radius: 20px;
+    border: solid 1px #000;
+    padding: 1.5rem;
+    width: 150px;
+`
+
+export const Span = styled.span`
+    font-size:0.8rem;
+    color:#000;
+`
+
+export const Tr = styled.tr`
+    border-radius: 20px;
+    box-shadow: 0px 0px 20px 1px grey;
+`
+
+export const Th = styled.th`
+    min-width: 110px;
+    padding: 8px;
+`
+
+export const Table = styled.table`
+    width: 100%;
+    border-spacing: 0 15px;
+`
+
+export const H2 = styled.h2`
+    margin: 0px;
+    padding: 0px;
+    font-size: 1.5em;
+    font-weight: bold;
+    text-align: center;
 `
---
+++ src/components/LogCard/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Td, Image } from './styles'
+
+export const LogCard = ({ data }) =>{
+
+    const getDate = () => {
+        console.log(data.createdAt)
+        const date = new Date(parseInt(data.createdAt))
+        return date.toLocaleDateString()
+    }
+
+    return (
+        <>
+            <Td>{data.user.firstName} {data.user.lastName}</Td>
+            <Td>{data.action}</Td>
+            <Td>{getDate()}</Td>
+        </>
+    )
+}
